f154e6aee63e753a6526e274a815187cdbba1eed
Matt.Faubel <matt.faubel@gmail.com>
2019-05-15 20:57:10 -0500

    Updates

---
+++ precache-manifest.82b4304c52dd00e569f77b4a12c22692.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c81fdbc7997b8c2fa05e4ec52084c9e0",
+    "url": "/react-videos/index.html"
+  },
+  {
+    "revision": "172ab3deebbf01d2d788",
+    "url": "/react-videos/static/css/main.b97561cc.chunk.css"
+  },
+  {
+    "revision": "491a1c1d29ef0ed8904c",
+    "url": "/react-videos/static/js/2.0eeba5c1.chunk.js"
+  },
+  {
+    "revision": "172ab3deebbf01d2d788",
+    "url": "/react-videos/static/js/main.8de81c04.chunk.js"
+  },
+  {
+    "revision": "ad827d23e73279ca75eb",
+    "url": "/react-videos/static/js/runtime~main.f892f45d.js"
+  }
+]);
