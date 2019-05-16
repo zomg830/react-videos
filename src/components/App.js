f3fed15584dc457fe5b783555fdc78769db19f69
zomg830 <matt.faubel@gmail.com>
2019-05-15 20:41:35 -0500

    Cleaned up unused components

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,6 @@ import SearchBar from "./SearchBar";
 import youtube from "../apis/youtube";
 import VideoList from "./VideoList";
 import VideoDetail from "./VideoDetail";
-import GoogleAuth from "./GoogleAuth";
 
 class App extends React.Component {
   state = { videos: [], selectedVideo: null };
